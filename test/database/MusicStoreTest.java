6fb61d7700161ece9d95630acdd92abe358682fb
KrisKlindt <kklindt@arizona.edu>
2025-02-18 09:24:07 -0800

    Skeleton for structure

---
+++ test/database/MusicStoreTest.java
@@ -0,0 +1,14 @@
+package database;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MusicStoreTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
